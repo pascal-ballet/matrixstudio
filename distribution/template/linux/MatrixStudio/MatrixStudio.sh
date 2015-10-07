868e9ec3be7557dd78c7635962e7319b12b5c9d2
Jean-Charles Roger <jeancharles.roger@me.com>
2015-10-07 10:05:10 +0200

    Distribution is now compatible with gradle.

--- distribution/template/linux/MatrixStudio/MatrixStudio.sh
+++ distribution/template/linux/MatrixStudio/MatrixStudio.sh
@@ -3,4 +3,4 @@ JAVA=java
 BASEDIR=`dirname "$0"`
 VMOPTIONS="-Xmx512m"
 OPTIONS=""
-exec $JAVA $VMOPTIONS -Djava.library.path=$BASEDIR/jars -jar jars/MatrixStudio.jar $OPTIONS
+exec $JAVA $VMOPTIONS -Djava.library.path=$BASEDIR/jars -jar jars/ui-0.8.jar $OPTIONS
