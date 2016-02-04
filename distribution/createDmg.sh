ebd72995a1d911e560104a4bb095bd9dc7a29b47
Jean-Charles Roger <jeancharles.roger@me.com>
2016-02-04 11:19:41 +0100

    Updates build for drone

--- distribution/createDmg.sh
+++ distribution/createDmg.sh
@@ -2,8 +2,8 @@
 #Creates the DMG files for MatrixStudio in 32bits and 64bits.
 
 
-VERSION=`cat ../out/production/ui/matrixstudio/ui/version.properties | grep version | awk -F= '{ print $2 }'`
-DATE=`cat ../out/production/ui/matrixstudio/ui/version.properties | grep date | awk -F= '{ print $2 }'`
+VERSION=`cat ../ui/src/main/resources/matrixstudio/ui/version.properties | grep version | awk -F= '{ print $2 }'`
+DATE=`cat ../ui/src/main/resources/matrixstudio/ui/version.properties | grep date | awk -F= '{ print $2 }'`
 
 DIST=built
 
@@ -16,17 +16,17 @@ else
 fi
 
 if ( test -d $DIST/macosx32 ); then
-	echo "Creating $DIST/MatrixStudio-$TAG-macosx-32bits.dmg"
-	if ( test -f $DIST/MatrixStudio-$TAG-macosx-32bits.dmg ); then
- 		rm  $DIST/MatrixStudio-$TAG-macosx-32bits.dmg
+	echo "Creating $DIST/MatrixStudio-$TAG-macosx-32.dmg"
+	if ( test -f $DIST/MatrixStudio-$TAG-macosx-32.dmg ); then
+ 		rm  $DIST/MatrixStudio-$TAG-macosx-32.dmg
  	fi
-	hdiutil create -srcfolder $DIST/macosx32 -volname MatrixStudio $DIST/MatrixStudio-$TAG-macosx-32bits.dmg
+	hdiutil create -srcfolder $DIST/macosx32 -volname MatrixStudio $DIST/MatrixStudio-$TAG-macosx-32.dmg
 fi
 
 if ( test -d $DIST/macosx64 ); then
-	echo "Creating $DIST/MatrixStudio-$TAG-macosx-64bits.dmg"
-	if ( test -f $DIST/MatrixStudio-$TAG-macosx-64bits.dmg ); then
- 		rm  $DIST/MatrixStudio-$TAG-macosx-64bits.dmg 
+	echo "Creating $DIST/MatrixStudio-$TAG-macosx-64.dmg"
+	if ( test -f $DIST/MatrixStudio-$TAG-macosx-64.dmg ); then
+ 		rm  $DIST/MatrixStudio-$TAG-macosx-64.dmg
  	fi
-	hdiutil create -srcfolder $DIST/macosx64 -volname MatrixStudio $DIST/MatrixStudio-$TAG-macosx-64bits.dmg
+	hdiutil create -srcfolder $DIST/macosx64 -volname MatrixStudio $DIST/MatrixStudio-$TAG-macosx-64.dmg
 fi
