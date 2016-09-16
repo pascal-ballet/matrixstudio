376aebb1ecfc63402e48f7b3c8ece33ecb2c365b
Jean-Charles Roger <jeancharles.roger@me.com>
2016-09-16 17:15:52 +0200

    Adds tests

--- core/src/test/java/matrixstudio/formula/FormulaParserTest.java
+++ core/src/test/java/matrixstudio/formula/FormulaParserTest.java
@@ -1,5 +1,6 @@
 package matrixstudio.formula;
 
+import matrixstudio.formula.BinaryOperation.Operation;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,27 +11,81 @@ public class FormulaParserTest {
     @Test
     public void parseReference() throws Exception {
         Formula result = new FormulaParser("result").parse();
-        Assert.assertEquals(result.getClass(), Reference.class);
-        Assert.assertEquals(((Reference) result).getName(), "result");
+        Assert.assertTrue(result.equals(new Reference("result")));
 
         result = new FormulaParser("otherName").parse();
-        Assert.assertEquals(result.getClass(), Reference.class);
-        Assert.assertEquals(((Reference) result).getName(), "otherName");
+        Assert.assertTrue(result.equals(new Reference("otherName")));
     }
 
     @Test
     public void parseLiteral() throws Exception {
         Formula result = new FormulaParser("123").parse();
-        Assert.assertEquals(result.getClass(), Literal.class);
-        Assert.assertEquals((long) ((Literal) result).getValue(), 123l);
+        Assert.assertTrue(result.equals(new Literal(123l)));
 
         result = new FormulaParser("+4567").parse();
-        Assert.assertEquals(result.getClass(), Literal.class);
-        Assert.assertEquals((long) ((Literal) result).getValue(), 4567l);
+        Assert.assertTrue(result.equals(new Literal(4567l)));
 
         result = new FormulaParser("-8910").parse();
-        Assert.assertEquals(result.getClass(), Literal.class);
-        Assert.assertEquals((long) ((Literal) result).getValue(), -8910l);
+        Assert.assertTrue(result.equals(new Literal(-8910l)));
+    }
+
+    @Test
+    public void parseSubFormula() throws Exception {
+        Formula result = new FormulaParser("(123)").parse();
+        Assert.assertTrue(result.equals(new SubFormula(new Literal(123l))));
+
+        result = new FormulaParser("(otherName + 4 + a)").parse();
+        Assert.assertTrue(result.equals(
+            new SubFormula(
+                new BinaryOperation(Operation.Plus,
+                    new Reference("otherName"),
+                    new BinaryOperation(
+                            Operation.Plus,
+                            new Literal(4l),
+                            new Reference("a")
+                    )
+                )
+            )
+        ));
+
+
+        result = new FormulaParser("(1-456)").parse();
+        Assert.assertTrue(result.equals(new SubFormula(new BinaryOperation(Operation.Minus, new Literal(1l),new Literal(456l)))));
+
+        result = new FormulaParser("(1 - (a + b))").parse();
+        Assert.assertTrue(result.equals(
+            new SubFormula(
+                new BinaryOperation(Operation.Minus,
+                    new Literal(1l),
+                    new SubFormula(
+                        new BinaryOperation(
+                            Operation.Plus,
+                            new Reference("a"),
+                            new Reference("b")
+                        )
+                    )
+                )
+            )
+        ));
+    }
+
+    @Test
+    public void parseBinary() throws Exception {
+        Formula result = new FormulaParser("1 + a").parse();
+        Assert.assertTrue(result.equals(new BinaryOperation(Operation.Plus, new Literal(1l), new Reference("a"))));
+
+        result = new FormulaParser("1 - a").parse();
+        Assert.assertTrue(result.equals(new BinaryOperation(Operation.Minus, new Literal(1l), new Reference("a"))));
+
+        result = new FormulaParser("1 * a").parse();
+        Assert.assertTrue(result.equals(new BinaryOperation(Operation.Multiply, new Literal(1l), new Reference("a"))));
+
+        result = new FormulaParser("1 / a").parse();
+        Assert.assertTrue(result.equals(new BinaryOperation(Operation.Divide, new Literal(1l), new Reference("a"))));
+
+        result = new FormulaParser("1 % a").parse();
+        Assert.assertTrue(result.equals(new BinaryOperation(Operation.Modulus, new Literal(1l), new Reference("a"))));
+
     }
 
 }
