da36348511ac67d69ca5c71b9a77c92187a3ee56
Jean-Charles Roger <jeancharles.roger@me.com>
2016-09-17 22:30:38 +0200

    Model controller

---
+++ ui/src/main/java/matrixstudio/ui/controller/ModelController.java
@@ -0,0 +1,15 @@
+package matrixstudio.ui.controller;
+
+import matrixstudio.model.Model;
+import org.xid.basics.ui.controller.Controller;
+import org.xid.basics.ui.field.CompositeField;
+
+/**
+ * Controller for a model
+ */
+public class ModelController extends Controller<Model> {
+    @Override
+    public CompositeField createFields() {
+        return null;
+    }
+}
