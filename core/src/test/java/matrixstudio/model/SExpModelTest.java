6e5b88013f066705b92397f05610a6f33ea72a6e
Jean-Charles <jeancharles.roger@me.com>
2018-04-28 12:31:13 +0200

    Adds matrices tests (doesn't pass yet)

--- core/src/test/java/matrixstudio/model/SExpModelTest.java
+++ core/src/test/java/matrixstudio/model/SExpModelTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.BiFunction;
+import matrixstudio.formula.FormulaCache;
 import matrixstudio.kernel.SExpModelLoader;
 import matrixstudio.kernel.SExpModelSaver;
 import org.junit.Assert;
@@ -36,12 +38,20 @@ public class SExpModelTest {
 
 		Model source = new Model();
 		source.addParameterAndOpposite(createParameter("p1", "42"));
+		source.addParameterAndOpposite(createParameter("p2", "42+42"));
 		source.addCodeAndOpposite(createLibrary("Library", "// Code here"));
 		source.addCodeAndOpposite(createKernel("Kernel1", "// Kernel 1"));
 		source.addCodeAndOpposite(createKernel("Kernel2", "// Kernel 2"));
 		createScheduler(source);
 
+		source.addMatrixAndOpposite(createMatrixInteger(50, 50, (i,j) -> i+j));
+		source.addMatrixAndOpposite(createMatrixFloat(50, 50, (i,j) -> (float) Math.sqrt(i+j)));
+
 		Model loaded = loadAndSave(source);
+
+		Assert.assertEquals(2, loaded.getParameterCount());
+		Assert.assertEquals(42, FormulaCache.SHARED.computeValue(loaded.getParameter(0).getFormula(), loaded));
+		Assert.assertEquals(84, FormulaCache.SHARED.computeValue(loaded.getParameter(1).getFormula(), loaded));
 
 		Assert.assertEquals(3, loaded.getCodeCount());
 		Assert.assertEquals("Library", loaded.getCode(0).getName());
@@ -49,12 +59,17 @@ public class SExpModelTest {
 		Assert.assertEquals("Kernel2", loaded.getCode(2).getName());
 		Assert.assertEquals("// Kernel 2", loaded.getCode(2).getContents());
 
+		Assert.assertEquals(2, loaded.getMatrixCount());
+		Assert.assertEquals(30, loaded.getMatrix(0).getInitValueAt(10, 20, 0));
+		Assert.assertEquals( Math.sqrt(30), loaded.getMatrix(1).getInitValueAt(10, 20, 0).floatValue(), 1e-5);
+
 		Assert.assertEquals(4, loaded.getScheduler().getTaskCount());
 		Assert.assertEquals(2, loaded.getScheduler().getTask(0).getTaskOutCount());
 		Assert.assertEquals(2, loaded.getScheduler().getTask(3).getTaskInCount());
 		Assert.assertEquals(1, loaded.getScheduler().getTask(2).getKernelCount());
 		Assert.assertEquals(300f, loaded.getScheduler().getTask(1).getPosition()[0], 1e-5);
 		Assert.assertEquals(100f, loaded.getScheduler().getTask(1).getPosition()[1], 1e-5);
+
 	}
 
 	private Parameter createParameter(String name, String formula) {
@@ -76,6 +91,34 @@ public class SExpModelTest {
 		library.setName(name);
 		library.setContents(contents);
 		return library;
+	}
+
+	protected MatrixInteger createMatrixInteger(int x, int y, BiFunction<Integer, Integer, Integer> filler) {
+		MatrixInteger matrix = new MatrixInteger();
+		matrix.setSizeX(Integer.toString(x));
+		matrix.setSizeY(Integer.toString(y));
+		matrix.setSizeZ(Integer.toString(1));
+		matrix.initBlank(true);
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				matrix.setInitValueAt(i, j, 0, filler.apply(i, j));
+			}
+		}
+		return matrix;
+	}
+
+	protected MatrixFloat createMatrixFloat(int x, int y, BiFunction<Integer, Integer, Float> filler) {
+		MatrixFloat matrix = new MatrixFloat();
+		matrix.setSizeX(Integer.toString(x));
+		matrix.setSizeY(Integer.toString(y));
+		matrix.setSizeZ(Integer.toString(1));
+		matrix.initBlank(true);
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				matrix.setInitValueAt(i, j, 0, filler.apply(i, j));
+			}
+		}
+		return matrix;
 	}
 
 	private void createScheduler(Model model) {
