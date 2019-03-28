67594a342cdd979e257c4b1699bbda683b1d680f
Jean-Charles <jeancharles.roger@me.com>
2019-03-28 15:54:22 +0100

    Correct redraw disposed shells

--- ui/src/main/java/matrixstudio/ui/MatrixStudio.java
+++ ui/src/main/java/matrixstudio/ui/MatrixStudio.java
@@ -38,17 +38,18 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-
 public class MatrixStudio implements SimulatorContext, StudioContext {
 
 	private String version;
 	private String date;
 
-	/** Stores last path to select files. */
+	/**
+	 * Stores last path to select files.
+	 */
 	private Path currentPath = Paths.get(".");
 	private Path modelPath;
 	private Model model;
-	
+
 	private Display display;
 	private Shell shell;
 
@@ -57,13 +58,13 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 
 	private FieldShellToolkit toolkit;
 	private Actions actions;
-	
+
 	private MSResources resources;
-	
+
 	private BorderField mainField;
-	
+
 	private MultiTabField centerField;
-	
+
 	private MatrixTabController matrixTabController;
 	private ScheduleTabController schedulerTabController;
 	private KernelTabController kernelTabController;
@@ -71,13 +72,13 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 
 	private BorderField southField;
 
-    private ModelController modelController;
+	private ModelController modelController;
 
-    private MultiTabField southEastField;
-    private CompositeField consoleComposite;
+	private MultiTabField southEastField;
+	private CompositeField consoleComposite;
 	private ConsoleField consoleField;
 	private int consoleIndex;
-	
+
 	private CompositeField propertiesComposite;
 	private PropertiesField propertiesField;
 
@@ -86,15 +87,16 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	private ActionExecuter executer = new ActionExecuter() {
 		public void executeAction(Action action) {
 			boolean transactional = action.hasStyle(Action.STYLE_TRANSACTIONNAL);
-			if ( transactional ) {
+			if (transactional) {
 				model.getChangeRecorder().newOperation();
 			}
 
 			int status = action.run(ActionMonitor.empty);
-			if ( transactional && status == Action.STATUS_CANCEL ) {
+			if (transactional && status == Action.STATUS_CANCEL) {
 				model.getChangeRecorder().undo();
 			}
-			if (!shell.isDisposed()) refreshFields();
+			if (!shell.isDisposed())
+				refreshFields();
 		}
 	};
 
@@ -103,7 +105,7 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 		model = Tools.createEmptyModel();
 		simulator = new Simulator(this);
 	}
-	
+
 	private void initVersion() {
 		try {
 			Properties properties = new Properties();
@@ -111,31 +113,32 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			version = properties.getProperty("version");
 			date = properties.getProperty("date");
 		} catch (Exception e) {
-			version="unknown";
-			date="unknown";
+			version = "unknown";
+			date = "unknown";
 		}
 	}
 
 	public boolean GetEmbedded() {
 		return simulator.Embedded;
 	}
+
 	public void SetEmbedded(boolean b) {
 		simulator.Embedded = b;
 	}
 
-	private boolean compiled=false;
+	private boolean compiled = false;
 
 	public CompositeField createFields() {
 		centerField = new MultiTabField();
-		
+
 		createMatricesTab();
 		createSchedulerTab();
 		createKernelsTab();
 		createLibrariesTab();
-		
+
 		centerField.addStructureListener(new NotificationListener() {
 			public void notified(Notification n) {
-				if (BasicsUI.NOTIFICATION_SELECTION.equals(n.name) ) {
+				if (BasicsUI.NOTIFICATION_SELECTION.equals(n.name)) {
 					setSelection(null);
 					refreshFields();
 				}
@@ -144,14 +147,14 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 
 		createSouthEastTab();
 		createPropertiesTab();
-		
+
 		southField = new BorderField(BasicsUI.NONE, southEastField);
 		southField.setWest(propertiesComposite, 60);
-		
+
 		mainField = new BorderField(BasicsUI.NONE, centerField);
 		mainField.setSouth(southField, 30);
 		mainField.setActionExecuter(executer);
-		
+
 		return mainField;
 	}
 
@@ -178,18 +181,18 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 		libraryTabController.setSubject(model);
 		centerField.addTab(libraryTabController, resources.getImage("eclipse/outline_co.gif"), false);
 	}
-	
-	private void createSouthEastTab() {
-        southEastField = new MultiTabField();
-        southEastField.setActionExecuter(executer);
 
-        modelController = new ModelController();
-        modelController.setSubject(model);
-        southEastField.addTab(modelController, resources.getImage("eclipse/file_obj.gif"), false);
+	private void createSouthEastTab() {
+		southEastField = new MultiTabField();
+		southEastField.setActionExecuter(executer);
+
+		modelController = new ModelController();
+		modelController.setSubject(model);
+		southEastField.addTab(modelController, resources.getImage("eclipse/file_obj.gif"), false);
 
 		consoleField = new ConsoleField();
 		consoleComposite = new CompositeField("Console", consoleField);
-        southEastField.addTab(consoleComposite, resources.getImage("eclipse/console_view.gif"), false);
+		southEastField.addTab(consoleComposite, resources.getImage("eclipse/console_view.gif"), false);
 
 		consoleIndex = southEastField.indexOf(consoleComposite);
 	}
@@ -204,7 +207,7 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	public MultiTabField getCenterField() {
 		return centerField;
 	}
-	
+
 	public boolean isTextEditing() {
 		// Careful is tab order changes.
 		int selected = centerField.getSelected();
@@ -212,9 +215,10 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	}
 
 	public void refreshFields() {
-		if ( model == null ) {
+		if (model == null) {
 			mainField.setEnable(false);
-		} else {
+		}
+		else {
 			mainField.setEnable(true);
 
 			refreshMatrixShells();
@@ -223,23 +227,24 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			// Careful is tab order changes.
 			int selected = centerField.getSelected();
 			switch (selected) {
-			case 0:
-				refreshMatrices();
-				break;
-			case 1:
-				refreshSchedule();
-				break;
-			case 2:
-				refreshKernels();
-				break;
-			case 3:
-				refreshLibrary();
-				break;
+				case 0:
+					refreshMatrices();
+					break;
+				case 1:
+					refreshSchedule();
+					break;
+				case 2:
+					refreshKernels();
+					break;
+				case 3:
+					refreshLibrary();
+					break;
 			}
 
 			refreshModel();
 
-			if (toolkit != null ) toolkit.validateAll();
+			if (toolkit != null)
+				toolkit.validateAll();
 		}
 	}
 
@@ -247,7 +252,7 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 		matrixTabController.setSubject(model);
 		matrixTabController.refreshFields();
 	}
-	
+
 	public void refreshSchedule() {
 		schedulerTabController.setSubject(model);
 		schedulerTabController.refreshFields();
@@ -264,21 +269,22 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	}
 
 	public void refreshModel() {
-        modelController.setSubject(model);
-        modelController.refreshFields();
-    }
+		modelController.setSubject(model);
+		modelController.refreshFields();
+	}
 
-    public void redrawMatrixShells() {
+	public void redrawMatrixShells() {
 		for (Shell shell : matrixShells.values()) {
-			if (!shell.isDisposed()) shell.redraw();
+			if (!shell.isDisposed())
+				shell.redraw();
 		}
 	}
 
-    public void refreshMatrixShells() {
+	public void refreshMatrixShells() {
 		List<Matrix> toClose = new ArrayList<>();
 		for (Map.Entry<Matrix, Shell> entry : matrixShells.entrySet()) {
 			Matrix matrix = entry.getKey();
-			if (model.getMatrixList().contains(matrix)) {
+			if (model.getMatrixList().contains(matrix) && shell != null && !shell.isDisposed()) {
 				Shell shell = entry.getValue();
 				if (!matrix.getName().equals(shell.getText())) {
 					shell.setText(matrix.getName());
@@ -288,7 +294,8 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 					shell.setSize(matrix.safeGetSizeXValue(), matrix.safeGetSizeYValue());
 				}
 				shell.redraw();
-			} else {
+			}
+			else {
 				toClose.add(matrix);
 			}
 		}
@@ -298,16 +305,18 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 		}
 	}
 
-    @Override
-    public void asynchronousRun(int milliseconds, Runnable runnable) {
-        if ( display.isDisposed() ) return;
+	@Override
+	public void asynchronousRun(int milliseconds, Runnable runnable) {
+		if (display.isDisposed())
+			return;
 
-        if ( milliseconds <= 0 ) {
-            display.asyncExec(runnable);
-        } else {
-            display.timerExec(milliseconds, runnable);
-        }
-    }
+		if (milliseconds <= 0) {
+			display.asyncExec(runnable);
+		}
+		else {
+			display.timerExec(milliseconds, runnable);
+		}
+	}
 
 	@Override
 	public void simulationRefresh() {
@@ -318,19 +327,19 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 		return display;
 	}
 
-    @Override
+	@Override
 	public Shell getShell() {
 		return shell;
 	}
-	
+
 	@Override
-    public Object getSelection() {
+	public Object getSelection() {
 		return propertiesField.getSelected();
 	}
-	
+
 	@Override
-    public void setSelection(Object selection) {
-		if ( propertiesField != null ) {
+	public void setSelection(Object selection) {
+		if (propertiesField != null) {
 			propertiesField.setSubject(selection);
 		}
 	}
@@ -338,35 +347,35 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	public Path getModelPath() {
 		return modelPath;
 	}
-	
+
 	public Model getModel() {
 		return model;
 	}
-	
+
 	@Override
-    public Simulator getSimulator() {
+	public Simulator getSimulator() {
 		return simulator;
 	}
 
-    @Override
-    public boolean isCompiled() {
+	@Override
+	public boolean isCompiled() {
 		return compiled;
 	}
-	
+
 	@Override
-    public void setCompiled(boolean compiled) {
+	public void setCompiled(boolean compiled) {
 		this.compiled = compiled;
 	}
-	
+
 	@Override
-    public MSResources getResources() {
+	public MSResources getResources() {
 		return resources;
 	}
-	
+
 	public ChangeRecorder getChangeRecorder() {
 		return model.getChangeRecorder();
 	}
-	
+
 	public void resetModel() {
 		model = Tools.createEmptyModel();
 		modelPath = null;
@@ -375,14 +384,16 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	public boolean loadModel(boolean safeRead) {
 		final FileDialog dialog = new FileDialog(shell, SWT.OPEN);
 		dialog.setText("Open a simulation.");
-        dialog.setFilterExtensions(new String[] { "*.mss", SExpModel.MATRIXSTUDIO_SIMULATION, "*.*" });
-        dialog.setFilterNames(new String[] { "Matrix Studio Simulation", "Exploded Matrix Studio Simulation", "Other files" });
-        dialog.setFilterPath(currentPath.toString());
-        String resultFilename = dialog.open();
-        if( resultFilename==null ) return false;
+		dialog.setFilterExtensions(new String[] { "*.mss", SExpModel.MATRIXSTUDIO_SIMULATION, "*.*" });
+		dialog.setFilterNames(new String[] { "Matrix Studio Simulation", "Exploded Matrix Studio Simulation", "Other files" });
+		dialog.setFilterPath(currentPath.toString());
+		String resultFilename = dialog.open();
+		if (resultFilename == null)
+			return false;
 
 		Path path = Paths.get(resultFilename);
-		if( !Files.exists(path) ) return false;
+		if (!Files.exists(path))
+			return false;
 
 		currentPath = path.getParent();
 		stopAllExports();
@@ -392,76 +403,81 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			model = Tools.load(path);
 			modelPath = path;
 		} catch (IOException e) {
-			error("Cannot file simulation from file '"+ path +"': " + e.getMessage());
+			error("Cannot file simulation from file '" + path + "': " + e.getMessage());
 			return false;
 		}
-		log("Simulation successfully loaded from file '"+ path +"'.");
+		log("Simulation successfully loaded from file '" + path + "'.");
 		return true;
 	}
 
 	@Override
-    public boolean saveModel(boolean forceDialog) {
-		if ( model == null ) return false;
+	public boolean saveModel(boolean forceDialog) {
+		if (model == null)
+			return false;
 
 		Path path = modelPath;
-		if (forceDialog || modelPath == null ) {
+		if (forceDialog || modelPath == null) {
 			// selects a file where to file
 			final FileDialog dialog = new FileDialog(shell, SWT.SAVE);
-	        dialog.setText("Save the simulation as...");
-	        dialog.setFilterExtensions(new String[] { "*.mss", SExpModel.MATRIXSTUDIO_SIMULATION, "*.*" });
+			dialog.setText("Save the simulation as...");
+			dialog.setFilterExtensions(new String[] { "*.mss", SExpModel.MATRIXSTUDIO_SIMULATION, "*.*" });
 			dialog.setFilterNames(new String[] { "Matrix Studio Simulation", "Exploded Matrix Studio Simulation", "Other files" });
 			dialog.setFilterPath(currentPath.toString());
-	        
-	        String resultFilename = dialog.open();
-	        if ( resultFilename == null ) return false;
 
-	        path = Paths.get(resultFilename);
-	        if (dialog.getFilterIndex() == 1) {
+			String resultFilename = dialog.open();
+			if (resultFilename == null)
+				return false;
+
+			path = Paths.get(resultFilename);
+			if (dialog.getFilterIndex() == 1) {
 				if (Files.isDirectory(path)) {
 					path = path.resolve(SExpModel.MATRIXSTUDIO_SIMULATION);
-				} else {
+				}
+				else {
 					path = path.getParent().resolve(SExpModel.MATRIXSTUDIO_SIMULATION);
 				}
-			} else if( dialog.getFilterIndex() == 1 && !resultFilename.endsWith(".mss") ) {
-	        	path = path.getParent().resolve(path.getFileName().toString() + ".mss");
-	        }
+			}
+			else if (dialog.getFilterIndex() == 1 && !resultFilename.endsWith(".mss")) {
+				path = path.getParent().resolve(path.getFileName().toString() + ".mss");
+			}
 
-	        currentPath = path.getParent();
+			currentPath = path.getParent();
 		}
-		
+
 		try {
 			// saves model
 			Tools.save(model, path);
 			modelPath = path;
 		} catch (IOException e) {
-			error("Cannot save simulation to file '"+ path +"': " + e.getMessage());
+			error("Cannot save simulation to file '" + path + "': " + e.getMessage());
 			return false;
 		}
-		log("Simulation successfully saved to file '"+ path + "'.");
-		return true;	
+		log("Simulation successfully saved to file '" + path + "'.");
+		return true;
 	}
-	
-	
+
 	public void openFindAndReplaceDialog() {
 		// Careful is tab order changes.
 		int selected = centerField.getSelected();
-		if ( selected == 2 ) {
+		if (selected == 2) {
 			kernelTabController.openFindAndReplaceDialog();
-		} else if ( selected == 3 ) {
+		}
+		else if (selected == 3) {
 			libraryTabController.openFindAndReplaceDialog();
 		}
-	
+
 	}
-	
+
 	public void log(final String message) {
 		display.asyncExec(() -> {
 			int index = southEastField.getSelected();
-			if (index != consoleIndex) southEastField.setSelected(consoleIndex);
+			if (index != consoleIndex)
+				southEastField.setSelected(consoleIndex);
 			consoleField.log(message + "\n");
 			consoleField.scrollToTheEnd();
 		});
 	}
-	
+
 	public void warning(final String message) {
 		display.asyncExec(new Runnable() {
 			public void run() {
@@ -470,7 +486,7 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			}
 		});
 	}
-	
+
 	public void error(final String message) {
 		display.asyncExec(new Runnable() {
 			public void run() {
@@ -479,15 +495,17 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			}
 		});
 	}
-	
-	/** Sets output redirection to console. */
+
+	/**
+	 * Sets output redirection to console.
+	 */
 	private void initStdoutRedirection() {
 		PrintStream out = new PrintStream(new OutputStream() {
 
 			private StringBuilder buffer = null;
 
 			private void log(final String message) {
-				if ( !display.isDisposed() ) {
+				if (!display.isDisposed()) {
 					display.asyncExec(new Runnable() {
 						public void run() {
 							MatrixStudio.this.log(message);
@@ -495,55 +513,58 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 					});
 				}
 			}
+
 			@Override
 			public void write(int code) throws IOException {
-				if ( buffer == null ) {
+				if (buffer == null) {
 					buffer = new StringBuilder();
 				}
 				char toPrint = (char) code;
-				if ( toPrint == '\n' ) {
+				if (toPrint == '\n') {
 					log(buffer.toString());
 					buffer = null;
-				} else {
+				}
+				else {
 					buffer.append(toPrint);
 				}
 			}
-			
+
 			@Override
 			public void flush() throws IOException {
 				log(buffer.toString());
 				buffer = null;
 			}
-			
-		}) ;
+
+		});
 		System.setOut(out);
 	}
-	
+
 	public void open() {
-		
+
 		initStdoutRedirection();
-		
+
 		display = new Display();
 		resources = MSResources.getInstance(MSResources.class);
-		
+
 		CompositeField mainField = createFields();
 		refreshFields();
 		shell = FieldShellToolkit.createShell(display, createHeaderTitle());
 
 		actions = new Actions(this);
-		
+
 		String title = createHeaderTitle();
-		
-		toolkit = new FieldShellToolkit(shell, title , BasicsUI.SHOW_HINTS | BasicsUI.NO_AUTOMATIC_VALIDATION | BasicsUI.NO_HEADER, mainField);
+
+		toolkit = new FieldShellToolkit(shell, title, BasicsUI.SHOW_HINTS | BasicsUI.NO_AUTOMATIC_VALIDATION | BasicsUI.NO_HEADER,
+				mainField);
 		toolkit.setBannerImage(resources.getImage("MatrixStudio-32.png"));
 		toolkit.setActionExecuter(executer);
 		toolkit.setMenuActions(actions.getMenuBarAction().getActions());
 		toolkit.init();
 
-		PlatformUtil.registerCocoaNameAboutAndPreference("MatrixStudio", actions.getAction("About"),new Action.Stub());
-		
+		PlatformUtil.registerCocoaNameAboutAndPreference("MatrixStudio", actions.getAction("About"), new Action.Stub());
+
 		shell.setSize(1024, 768);
-		if(simulator.Embedded == false) {
+		if (simulator.Embedded == false) {
 			shell.open();
 			try {
 				while (!shell.isDisposed()) {
@@ -560,7 +581,6 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 			}
 		}
 	}
-
 
 	public Boolean isShellOpenForMatrix(Matrix matrix) {
 		Shell shell = matrixShells.get(matrix);
@@ -592,15 +612,14 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	/**
 	 * MatrixStudio header title construction.
 	 * It uses {@link CLUtil#isClPresent()} to print the information about CL.
+	 *
 	 * @return a string.
 	 */
 	private String createHeaderTitle() {
-        return
-                "M A T R I X   S T U D I O" + " - " +
-                "Version " + version + " of " + date + " - " +
-                "OpenCL device(s) " + (CLUtil.isClPresent() ? "FOUND." : "NOT FOUND. ");
+		return "M A T R I X   S T U D I O" + " - " + "Version " + version + " of " + date + " - " + "OpenCL device(s) " + (CLUtil
+				.isClPresent() ? "FOUND." : "NOT FOUND. ");
 	}
-	
+
 	public void recordMPEG() {
 		matrixTabController.recordMPEG();
 	}
@@ -608,17 +627,17 @@ public class MatrixStudio implements SimulatorContext, StudioContext {
 	public void recordPNG() {
 		matrixTabController.recordPNG();
 	}
-	
+
 	public void stopAllExports() {
 		matrixTabController.stopAllExports();
 	}
-	
+
 	private void dispose() {
 		matrixTabController.dispose();
 		MSResources.releaseInstance(MSResources.class);
 		simulator.stop();
 	}
-	
+
 	public static void main(String[] args) {
 		MatrixStudio ui = new MatrixStudio();
 		ui.open();
