8619d4cbb0c81c6d9099346124ac6b08c1b37f5a
Ballet <pascal.ballet@univ-brest.fr>
2021-03-16 10:37:04 +0100

    Useless initialisation removed. Huge performance improvement for big matrices (for ex. < 1s now when before it takes 6 min )

--- core/src/main/java/matrixstudio/kernel/SExpModelLoader.java
+++ core/src/main/java/matrixstudio/kernel/SExpModelLoader.java
@@ -284,7 +284,7 @@ public class SExpModelLoader {
 		final String toLoad = source;
 		postLoadingRunnables.add(() -> {
 			matrix.initBlank(true);
-			if (toLoad != null) {
+			/*if (toLoad != null) {
 				try {
 					ByteBuffer buffer = ByteBuffer.wrap(Files.readAllBytes(resolve(toLoad)));
 					int x = matrix.safeGetSizeXValue();
@@ -301,7 +301,7 @@ public class SExpModelLoader {
 				} catch (IOException e) {
 					// TODO present error
 				}
-			}
+			}*/
 			matrix.setToInitialValues();
 		});
 
